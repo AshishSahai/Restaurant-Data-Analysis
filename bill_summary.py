2715f82668295c5167403f0204f30ecce508c39a
Ashish Kumar <ashish0981kumar@gmail.com>
2025-04-13 18:06:32 +0530

    Initial commit - Restaurant Data Analysis

---
+++ bill_summary.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import seaborn as sns
+tips = sns.load_dataset("tips")
+#print(tips)
+
+df = pd.DataFrame(tips)
+#EDA - Exploratory Data Analysis
+print(df)
+print(df.head(10))
+print(df.info())
+print(df.describe())
+print("\n Missing values: \n", df.isnull().sum())
+print("\n Tip by day: \n", df.groupby("day",observed=True)["tip"].sum())
+tip_summary = df.groupby(["day","time"],observed=True)["tip"].sum()
+print("\nTip summary: \n", tip_summary)
+
+bill_summary_ex_tip = df.groupby(["day","time"],observed=True)["total_bill"].sum()
+print("\n bill_summary excluding tip: \n ", bill_summary_ex_tip)
+
+df["total_bill_in_tip"] = df["total_bill"]+df["tip"]
+print("bill including tip: \n",df["total_bill_in_tip"])
+print("\n bill summary including tip by day and time: \n", df.groupby(["day","time"],observed=True)["total_bill_in_tip"].sum())
+print("\n bill summary including tip by day: \n", df.groupby(["day"],observed=True)["total_bill_in_tip"].sum())
+best_day = df.groupby(["day"],observed=True)["total_bill_in_tip"].sum().idxmax()
+print("\n best day for earning is : ",best_day)
+worst_day = df.groupby(["day"],observed=True)["total_bill_in_tip"].sum().idxmin()
+print("\n Worst day for earning is : ", worst_day)
+
+df["tip_percent"] = (df["tip"]/df["total_bill"])*100
+print(df["tip_percent"])
+average_tip_percent = df.groupby("day", observed=True)["tip_percent"].mean()
+print("\n Average tip percent: ",df.groupby("day", observed=True)["tip_percent"].mean())
+print("\n Day with most generous customers : \n", average_tip_percent.idxmax())
